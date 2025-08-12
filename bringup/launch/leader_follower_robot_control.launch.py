8941a607c86f5a1bbb2acc9ee65aa4574a268fdb
dikeke <dikeke@qiniu.com>
2025-08-12 16:13:27 +0800

    fix

--- bringup/launch/leader_follower_robot_control.launch.py
+++ bringup/launch/leader_follower_robot_control.launch.py
@@ -29,6 +29,7 @@ def generate_launch_description():
     # Get controller config file path
     leader_controllers_file = os.path.join(pkg_share, 'config', 'leader_robot_controllers.yaml')
     
+    leader_controller_manager_name="/supre_robot_leader/controller_manager"
     leader_stack = GroupAction(
         actions=[
             PushRosNamespace('supre_robot_leader'),
@@ -53,35 +54,38 @@ def generate_launch_description():
             Node(
                 package='controller_manager',
                 executable='spawner',
-                arguments=['joint_state_broadcaster', '-c', '/controller_manager'],
+                arguments=['joint_state_broadcaster', '-c', leader_controller_manager_name],
             ),
             Node(
                 package='controller_manager',
                 executable='spawner',
-                arguments=['left_arm_controller', '-c', '/controller_manager'],
+                arguments=['left_arm_controller', '-c', leader_controller_manager_name],
             ),
     # Right arm controller spawner
             Node(
                 package="controller_manager",
                 executable="spawner",
-                arguments=["right_arm_controller", "--controller-manager", '/controller_manager'],
+                arguments=["right_arm_controller", "--controller-manager", leader_controller_manager_name],
             ),
 
         # Spawner for the main gripper controller
             Node(
                 package="controller_manager",
                 executable="spawner",
-                arguments=["misumi_gripper_controller", "--controller-manager", '/controller_manager'],
+                arguments=["misumi_gripper_controller", "--controller-manager", leader_controller_manager_name],
                 output="screen",
             )    
         ]
     )
 
-    leader_stack = GroupAction(
+    follower_controllers_file = os.path.join(pkg_share, 'config', 'follower_robot_controllers.yaml')
+
+    follower_controller_manager_name="/supre_robot_follower/controller_manager"
+
+    follower_stack = GroupAction(
         actions=[
-            PushRosNamespace('supre_robot_leader'),
+            PushRosNamespace('supre_robot_follower'),
             
-            # Leader Robot State Publisher
             Node(
                 package='robot_state_publisher',
                 executable='robot_state_publisher',
@@ -89,37 +93,34 @@ def generate_launch_description():
                 parameters=[robot_description]
             ),
             
-            # Leader Controller Manager
             Node(
                 package='controller_manager',
                 executable='ros2_control_node',
                 output='screen',
-                parameters=[robot_description, leader_controllers_file]
+                parameters=[robot_description, follower_controllers_file]
             ),
             
-            # Spawners for Leader
             Node(
                 package='controller_manager',
                 executable='spawner',
-                arguments=['joint_state_broadcaster', '-c', '/controller_manager'],
+                arguments=['joint_state_broadcaster', '-c', follower_controller_manager_name],
             ),
             Node(
                 package='controller_manager',
                 executable='spawner',
-                arguments=['left_arm_controller', '-c', '/controller_manager'],
+                arguments=['left_arm_controller', '-c', follower_controller_manager_name],
             ),
-    # Right arm controller spawner
             Node(
                 package="controller_manager",
                 executable="spawner",
-                arguments=["right_arm_controller", "--controller-manager", '/controller_manager'],
+                arguments=["right_arm_controller", "--controller-manager", follower_controller_manager_name],
             ),
 
         # Spawner for the main gripper controller
             Node(
                 package="controller_manager",
                 executable="spawner",
-                arguments=["misumi_gripper_controller", "--controller-manager", '/controller_manager'],
+                arguments=["misumi_gripper_controller", "--controller-manager", follower_controller_manager_name],
                 output="screen",
             )    
         ]
