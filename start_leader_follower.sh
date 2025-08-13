50bac2dadb8298011786159641cf9266135a8376
dikeke <dikeke@qiniu.com>
2025-08-13 19:31:55 +0800

    common

--- start_leader_follower.sh
+++ start_leader_follower.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 conda activate ros2_env
 source install/setup.bash
+sudo chmod 666 /dev/ttyTHS1
+export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libstdc++.so.6 python
 ros2 launch supre_robot_control  leader_follower_robot_control.launch.py
