8cb5cac88121c6202e6c0857ed8f53754cef8f47
dikeke <dikeke@qiniu.com>
2025-08-15 09:42:03 +0800

    leader follower

--- bringup/launch/leader_follower.launch.py
+++ bringup/launch/leader_follower.launch.py
@@ -9,37 +9,37 @@ def generate_launch_description():
     pkg_share = FindPackageShare('supre_robot_control')
     
    # --- Leader Launch ---
-    leader_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            PathJoinSubstitution([pkg_share, 'launch', 'common_robot_control.launch.py'])
-        ),
-        launch_arguments={
-            'namespace': 'supre_robot_leader',
-            'urdf_file_name': 'leader_supre_robot.urdf.xacro',
-            'controllers_file_name': 'leader_robot_controllers.yaml',
-            'robot_prefix': 'leader_',
-            'can_device_index': '0',
-            'serial_device': '/dev/ttyTHS1',
-            'serial_slave_id': '10'
-        }.items()
-    )
-
-#    follower_launch = IncludeLaunchDescription(
+#    leader_launch = IncludeLaunchDescription(
 #        PythonLaunchDescriptionSource(
 #            PathJoinSubstitution([pkg_share, 'launch', 'common_robot_control.launch.py'])
 #        ),
 #        launch_arguments={
-#            'namespace': 'supre_robot_follower',
-#            'urdf_file_name': 'follower_supre_robot.urdf.xacro',
-#            'controllers_file_name': 'follower_robot_controllers.yaml',
-#            'robot_prefix': 'follower_',
-#            'can_device_index': '1',
-#            'serial_device': '/dev/ttyTHS2',
+#            'namespace': 'supre_robot_leader',
+#            'urdf_file_name': 'leader_supre_robot.urdf.xacro',
+#            'controllers_file_name': 'leader_robot_controllers.yaml',
+#            'robot_prefix': 'leader_',
+#            'can_device_index': '0',
+#            'serial_device': '/dev/ttyTHS1',
 #            'serial_slave_id': '10'
 #        }.items()
 #    )
 
+    follower_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([pkg_share, 'launch', 'common_robot_control.launch.py'])
+        ),
+        launch_arguments={
+            'namespace': 'supre_robot_follower',
+            'urdf_file_name': 'follower_supre_robot.urdf.xacro',
+            'controllers_file_name': 'follower_robot_controllers.yaml',
+            'robot_prefix': 'follower_',
+            'can_device_index': '1',
+            'serial_device': '/dev/ttyTHS2',
+            'serial_slave_id': '10'
+        }.items()
+    )
+
     return LaunchDescription([
-        leader_launch,
-        #follower_launch,
+        #leader_launch,
+        follower_launch,
     ])
