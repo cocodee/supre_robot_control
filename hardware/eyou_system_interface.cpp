dbf1ccccde0d7e0209f7b6a60f39d5bf618254b3
dikeke <dikeke@qiniu.com>
2025-08-18 19:15:41 +0800

    test

--- hardware/eyou_system_interface.cpp
+++ hardware/eyou_system_interface.cpp
@@ -193,8 +193,8 @@ hardware_interface::return_type EyouSystemInterface::write(const rclcpp::Time & 
     for (size_t i = 0; i < motor_nodes_.size(); ++i) {
         if (hw_start_enabled_[i]) {
             // The sendCspTargetPosition function does not send the SYNC message itself.
-            motor_nodes_[i]->sendCspTargetPosition(hw_commands_positions_[i], 0, true);
-            RCLCPP_INFO(rclcpp::get_logger("EyouSystemInterface"), "send motor position %f, node_id:%d",hw_commands_positions_[i],motor_nodes_[i]->getNodeId());
+            int result = motor_nodes_[i]->sendCspTargetPosition(hw_commands_positions_[i], 0, true);
+            RCLCPP_INFO(rclcpp::get_logger("EyouSystemInterface"), "send motor position %f, node_id:%d, result:%d",hw_commands_positions_[i],motor_nodes_[i]->getNodeId(),result);
             any_motor_enabled = true;
             //auto error_code = motor_nodes_[i]->getErrorCode();
             //RCLCPP_INFO(rclcpp::get_logger("EyouSystemInterface"), "motor error code:%d",error_code);
