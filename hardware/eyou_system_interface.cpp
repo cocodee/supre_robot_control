ae1f8a24b859db7cd889385c1bffe9273f72f5e4
dikeke <dikeke@qiniu.com>
2025-09-09 09:19:52 +0800

    sync

--- hardware/eyou_system_interface.cpp
+++ hardware/eyou_system_interface.cpp
@@ -213,13 +213,13 @@ hardware_interface::return_type EyouSystemInterface::write(const rclcpp::Time & 
     for (size_t i = 0; i < motor_nodes_.size(); ++i) {
         if (hw_start_enabled_[i]) {
             // The sendCspTargetPosition function does not send the SYNC message itself.
-            int result = motor_nodes_[i]->sendCspTargetPosition(hw_commands_positions_[i], 0, true);
+            int result = motor_nodes_[i]->sendCspTargetPosition(hw_commands_positions_[i], 0, false);
             //RCLCPP_INFO(rclcpp::get_logger("EyouSystemInterface"), "send motor position %f, node_id:%d, result:%d",hw_commands_positions_[i],motor_nodes_[i]->getNodeId(),result);
             any_motor_enabled = true;
             //if(i%6==0){
             //    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-           // }
-           if (result != 0) {
+            //}
+            if (result != 0) {
                 RCLCPP_ERROR(rclcpp::get_logger("EyouSystemInterface"), "Failed to send motor position for joint %s", info_.joints[i].name.c_str());
                // return hardware_interface::return_type::ERROR;
             }
